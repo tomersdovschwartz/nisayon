6931327532a8f2cb31084ddd28500c6f29613ab1
תומר שוורץ <t1me4@hotmail.com>
2020-10-27 20:28:31 +0200

    test

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,4 +20,6 @@ public final class Constants {
     public static final int LEFT_BACK = 1;
     public static final int RIGHT_FRONT = 2;
     public static final int RIGHT_BACK = 3;
+    public static final int TEST = 3;
+    //testc
 }
